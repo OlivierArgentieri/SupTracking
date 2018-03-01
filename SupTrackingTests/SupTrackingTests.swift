5502623d8cb1f190b834dec3b678bd1fe00de459
Nunutte <nunutte@mac-de-khazam.local>
2018-03-01 03:57:56 +0100

    Initial Commit

---
+++ SupTrackingTests/SupTrackingTests.swift
@@ -0,0 +1,36 @@
+//
+//  SupTrackingTests.swift
+//  SupTrackingTests
+//
+//  Created by Nunutte on 01/03/2018.
+//  Copyright © 2018 Nunutte. All rights reserved.
+//
+
+import XCTest
+@testable import SupTracking
+
+class SupTrackingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
