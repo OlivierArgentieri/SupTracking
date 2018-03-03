540ef6c39b32ee0d9fc0d8d1bb3b95e731e715f7
Nunutte <nunutte@Mac-de-Khazam.local>
2018-03-03 22:10:28 +0100

    add post request json

--- SupTracking/RegisterVC.swift
+++ SupTracking/RegisterVC.swift
@@ -10,22 +10,6 @@ import UIKit
 
 class RegisterVC: UIViewController {
 
-    @IBOutlet weak var newUserLabel: UILabel!
-    @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var phoneLabel: UILabel!
-    @IBOutlet weak var firstNameLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var postalAddressLabel: UILabel!
-    
-    @IBOutlet weak var usernameText: UITextField!
-    @IBOutlet weak var passwordText: UITextField!
-    @IBOutlet weak var emailText: UITextField!
-    @IBOutlet weak var phoneText: UITextField!
-    @IBOutlet weak var firstnameText: UITextField!
-    @IBOutlet weak var nameText: UITextField!
-    @IBOutlet weak var postalAddressText: UITextField!
     
     var user:User? = nil
     
@@ -34,19 +18,18 @@ class RegisterVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+    /*
     @IBAction func ConfirmUserCreation(_ sender: Any) {
          self.user = User(Username: self.usernameText.text!, Password: self.passwordText.text!, Email:self.emailText.text!, PhoneNumber: self.phoneText.text!, LastName: self.nameText.text!, FirstName: self.firstnameText.text!, PostalAddress: self.postalAddressText.text!)
         performSegue(withIdentifier: "User", sender: user)
         
     }
-    
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        var DestViewController: ViewController = segue.destination as! ViewController
+    */
+    /*    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var DestViewController: ViewController =
         
         DestViewController.passedData = self.user
         
-    }
+    }*/
     
 }
--- SupTracking/ViewController.swift
+++ SupTracking/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelId: UILabel!
+    @IBOutlet weak var labelPassword: UILabel!
+    @IBOutlet weak var textId: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    @IBOutlet weak var btnConnexion: UIButton!
+    
+    var passedData: User! = nil
     override func viewDidLoad() {
         super.viewDidLoad()
-        var user:User = User(Username:"Nunutte", Password:"a", Email:"email@mail.com", PhoneNumber:"06000000", LastName:"lePrenom", FirstName:"LeNom", PostalAddress:"address")
         
+        self.textId.text = "admin"
+        self.textPassword.text = "admin"
+        /*var user:User = User(Username:"Nunutte", Password:"a", Email:"email@mail.com", PhoneNumber:"06000000", LastName:"lePrenom", FirstName:"LeNom", PostalAddress:"address")
+        */
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,22 +32,84 @@ class ViewController: UIViewController {
     }
 
     
-    @IBOutlet weak var labelId: UILabel!
-    @IBOutlet weak var labelPassword: UILabel!
-    @IBOutlet weak var textId: UITextField!
-    @IBOutlet weak var buttonConnection: UIButton!
-    @IBOutlet weak var buttonNewAccount: UIButton!
-    @IBOutlet weak var textPassword: UITextField!
-    
-    var passedData: User!
-  
+   
     @IBAction func buttonConnection(_ sender: Any) {
-        labelId.text = passedData.getUsername()
+        let username:String? = self.textId.text
+        let password:String? = self.textPassword.text
+        
+        
+        let headers = [
+            "contente-type" : "application/type",
+            "cache-control" : "no-cache"
+        ]
+        
+        let parameters = [
+            "login": "\(username)",
+            "password": "\(password)"
+        ]
+        
+        let url = URL(string: "http://supinfo.steve-colinet.fr/suptracking/")!
+        //var request = URLRequest(url: url!)
+        
+        //let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        //request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            let data = try? JSONSerialization.data(withJSONObject: parameters, options:.prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = data
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        /*
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        */
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+        /*
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response{
+                print(response)
+            }
+            
+            if let data = data{
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch{
+                    print(error)
+                }
+            }
+        }.resume()*/
+        //labelId.text = passedData.getUsername()
     }
     
-    @IBAction func touchDownNewAccount(_ sender: UIButton) {
-        
-    }
+   
     
     
     
