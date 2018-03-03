175b7682d9a05f65ca2e7abb4bf70a969cfed800
Nunutte <nunutte@Mac-de-Khazam.local>
2018-03-03 06:07:58 +0100

    Add : register View Controller

---
+++ SupTracking/RegisterVC.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterVC.swift
+//  SupTracking
+//
+//  Created by Nunutte on 03/03/2018.
+//  Copyright © 2018 Nunutte. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    @IBOutlet weak var newUserLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var postalAddressLabel: UILabel!
+    
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var phoneText: UITextField!
+    @IBOutlet weak var firstnameText: UITextField!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var postalAddressText: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
