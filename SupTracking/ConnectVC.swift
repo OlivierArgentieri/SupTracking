fc661b9c4c30711bf275a62ce0662caeed488cef
Nunutte <nunutte@Mac-de-Khazam.local>
2018-03-05 13:41:06 -0800

    Change on ConnectVC

--- SupTracking/ConnectVC.swift
+++ SupTracking/ConnectVC.swift
@@ -18,40 +18,50 @@ class ConnectVC: UIViewController {
     //var pin:MKAnnotation!
     
     var s:String!
+    var stopRefreshPosition:Bool!
     
     override func viewDidLoad() {
         //super.viewDidLoad()
+        self.stopRefreshPosition = false
         print(globalUser.sharedInstance.isConnected())
         if(globalUser.sharedInstance.isConnected()){
             
             self.CordLabel.text =  globalUser.sharedInstance.getGlobalUser().getUsername()
-            
-            useAPI.getCarPosition(login: globalUser.sharedInstance.getGlobalUser().getUsername(), password: globalUser.sharedInstance.getGlobalUser().getPassword()) { (completed, result) in
-                if (completed){
-                    if (result["success"] as? Bool)!{
-                        
-                        let position = result["position"] as? [String:Any]
-                        let latitudejson:Double = (position!["latitude"] as? Double)!
-                        let longitudejson:Double = (position!["longitude"] as? Double)!
-                        
-                        let coordinate = CLLocationCoordinate2D(latitude: latitudejson, longitude: longitudejson)
-                        let region = MKCoordinateRegionMakeWithDistance(coordinate, 1000, 1000)
-                        self.MKVMap.setRegion(region, animated: true)
-                        
-                        let annotation = MKPointAnnotation()
-                        
-                        annotation.coordinate = coordinate
-                        annotation.title = "Your car is here"
-                        self.MKVMap.addAnnotation(annotation)
+            DispatchQueue.global(qos: .background).async {
+                while(!self.stopRefreshPosition)
+                {
+                    
+                    sleep(1)
+                    print("Salut")
+                    useAPI.getCarPosition(login: globalUser.sharedInstance.getGlobalUser().getUsername(), password: globalUser.sharedInstance.getGlobalUser().getPassword()) { (completed, result) in
+                        if (completed){
+                            if (result["success"] as? Bool)!{
+                                
+                                let position = result["position"] as? [String:Any]
+                                let latitudejson:Double = (position!["latitude"] as? Double)!
+                                let longitudejson:Double = (position!["longitude"] as? Double)!
+                                
+                                let coordinate = CLLocationCoordinate2D(latitude: latitudejson, longitude: longitudejson)
+                                let region = MKCoordinateRegionMakeWithDistance(coordinate, 1000, 1000)
+                                self.MKVMap.setRegion(region, animated: true)
+                                
+                                let annotation = MKPointAnnotation()
+                                
+                                annotation.coordinate = coordinate
+                                annotation.title = "Your car is here"
+                                self.MKVMap.addAnnotation(annotation)
+                            }
+                        } else {
+                            // TODO
+                        }
                     }
-                } else {
-                    // TODO
                 }
             }
         }
     }
 
     @IBAction func OnClickLogout(_ sender: Any) {
-        globalUser.sharedInstance.killGlobalUser()
+        self.stopRefreshPosition = true
+        //globalUser.sharedInstance.killGlobalUser()
     }
 }
